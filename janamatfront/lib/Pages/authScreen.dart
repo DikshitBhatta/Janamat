131feb24f7617c060edc71f9b677d267529a11df
Dikshit Bhatta <147680213+DikshitBhatta@users.noreply.github.com>
2024-12-06 03:26:37 +0545

    authScreen

--- janamatfront/lib/Pages/authScreen.dart
+++ janamatfront/lib/Pages/authScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:janamatfront/providers/authenticationProvider.dart';
+import 'package:janamatfront/ocr/ocr.dart'; // Import OCRPage
 
 class AuthScreen extends StatefulWidget {
   @override
@@ -10,12 +11,17 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController(); // Add this controller
   bool _isSignUp = false;
+  bool _isPasswordVisible = false; // Add this state variable
+  bool _isOCRVerified = false; // Add this state variable
+  String _ocrErrorMessage = ''; // Add this state variable
 
   @override
   void dispose() {
     _emailController.dispose();
     _passwordController.dispose();
+    _confirmPasswordController.dispose(); // Dispose this controller
     super.dispose();
   }
 
@@ -24,50 +30,225 @@ class _AuthScreenState extends State<AuthScreen> {
     final authProvider = Provider.of<AuthenticationProvider>(context);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(_isSignUp ? 'Sign Up' : 'Sign In'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextFormField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            TextFormField(
-              controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                try {
-                  if (_isSignUp) {
-                    await authProvider.signUp(_emailController.text, _passwordController.text);
-                  } else {
-                    await authProvider.signIn(_emailController.text, _passwordController.text);
-                  }
-                } catch (e) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text(e.toString())),
-                  );
-                }
-              },
-              child: Text(_isSignUp ? 'Sign Up' : 'Sign In'),
-            ),
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  _isSignUp = !_isSignUp;
-                });
-              },
-              child: Text(_isSignUp ? 'Already have an account? Sign In' : 'Don\'t have an account? Sign Up'),
-            ),
-          ],
+      backgroundColor: const Color(0xFF0E2544), // Set the background color of the Scaffold
+      body: Container(
+        width: double.infinity,
+        // Remove the BoxDecoration here
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 100),
+              Text(
+                _isSignUp ? 'Sign Up' : 'Sign In',
+                style: const TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: Column(
+                  children: [
+                    _buildInputField(
+                      controller: _emailController,
+                      hintText: 'Email',
+                      icon: Icons.email,
+                    ),
+                    const SizedBox(height: 20),
+                    _buildInputField(
+                      controller: _passwordController,
+                      hintText: 'Password',
+                      icon: Icons.lock,
+                      obscureText: true,
+                    ),
+                    if (_isSignUp) ...[
+                      const SizedBox(height: 20),
+                      _buildInputField(
+                        controller: _confirmPasswordController,
+                        hintText: 'Confirm Password',
+                        icon: Icons.lock,
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () async {
+                          final result = await Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => OCRPage()),
+                          );
+                          if (result == true) {
+                            setState(() {
+                              _isOCRVerified = true;
+                              _ocrErrorMessage = '';
+                            });
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFFFFFFFF), // White button
+                          shadowColor: Colors.transparent,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 60,
+                            vertical: 15,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        child: const Text(
+                          'Citizenship Verification',
+                          style: TextStyle(
+                            color: Color(0xFF0E2544), // Theme color for text
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      if (_ocrErrorMessage.isNotEmpty)
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10.0),
+                          child: Text(
+                            _ocrErrorMessage,
+                            style: const TextStyle(
+                              color: Colors.red,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                    ],
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (_isSignUp) {
+                          if (_passwordController.text != _confirmPasswordController.text) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Passwords do not match')),
+                            );
+                            return;
+                          }
+                          if (!_isOCRVerified) {
+                            setState(() {
+                              _ocrErrorMessage = 'Citizenship verification is compulsory';
+                            });
+                            return;
+                          }
+                          await _signUp(authProvider);
+                        } else {
+                          await _signIn(authProvider);
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFFFFFFF), // White button
+                        shadowColor: Colors.transparent,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 80,
+                          vertical: 15,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      child: Text(
+                        _isSignUp ? 'Sign Up' : 'Sign In',
+                        style: const TextStyle(
+                          color: Color(0xFF0E2544), // Theme color for text
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          _isSignUp = !_isSignUp;
+                          _isOCRVerified = false; // Reset OCR verification status
+                          _ocrErrorMessage = ''; // Reset OCR error message
+                        });
+                      },
+                      child: Text(
+                        _isSignUp
+                            ? 'Already have an account? Sign In'
+                            : 'Don\'t have an account? Sign Up',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 50),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Future<void> _signUp(AuthenticationProvider authProvider) async {
+    try {
+      await authProvider.signUp(
+        _emailController.text,
+        _passwordController.text,
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(e.toString())),
+      );
+    }
+  }
+
+  Future<void> _signIn(AuthenticationProvider authProvider) async {
+    try {
+      await authProvider.signIn(
+        _emailController.text,
+        _passwordController.text,
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(e.toString())),
+      );
+    }
+  }
+
+  Widget _buildInputField({
+    required TextEditingController controller,
+    required String hintText,
+    required IconData icon,
+    bool obscureText = false,
+  }) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText && !_isPasswordVisible, // Modify this line
+      style: const TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+        prefixIcon: Icon(icon, color: Colors.white),
+        hintText: hintText,
+        hintStyle: const TextStyle(color: Colors.white54),
+        filled: true,
+        fillColor: Colors.white.withOpacity(0.1),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide.none,
+        ),
+        suffixIcon: obscureText
+            ? IconButton(
+                icon: Icon(
+                  _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _isPasswordVisible = !_isPasswordVisible;
+                  });
+                },
+              )
+            : null,
       ),
     );
   }
