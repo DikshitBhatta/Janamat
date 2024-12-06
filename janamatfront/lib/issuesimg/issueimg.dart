f9e85e6cbc430239c05eef0e62f0382aca379abe
Awasthi19 <sushilawasthi2999@gmail.com>
2024-12-06 08:59:03 +0545

    images

--- janamatfront/lib/issuesimg/issueimg.dart
+++ janamatfront/lib/issuesimg/issueimg.dart
@@ -1,10 +1,18 @@
 List<String> issueImages = [
-  'assets/issues/Watersupply.jpg', // Updated filename
-  'assets/issues/roadline.jpg',
-  'assets/issues/health.png',
-  'assets/issues/environment.png',
-  'assets/issues/electricity1.png',
-  'assets/issues/others.png',
+  'assets/issues/Watersupply.png', // Updated filename
+  'assets/issues/highway.png',
+  'assets/issues/healthcare.png',
+  'assets/issues/eco-friendly.png',
+  'assets/issues/tower.png',
+  'assets/issues/agriculture.png',
+  'assets/issues/liquid-soap.png',
+  'assets/issues/bus.png',
+  'assets/issues/protection.png',
+  'assets/issues/housing.png',  
+  'assets/issues/corruption.png',
+  'assets/issues/law.png',
+  'assets/issues/human-rights.png',
+  'assets/issues/animal-rights.png',
 ];
 
 List<String> issueLabels = [
